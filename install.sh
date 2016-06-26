ed0da9220e6e7e6783e392f20d8b12836a021f82
Frederic Schneider <frederic.schneider@rwth-aachen.de>
2016-06-26 04:43:26 +0200

    Added minimalistic tern- and gitconfig and updated install script.

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ rm -rf $HOME/.config/nvim* $HOME/.zsh* $HOME/.tmux*
 mkdir -p $HOME/.config/nvim
 
 # Symlink files
-ln -s $PWD/config/zsh $HOME/.zshrc
+# ln -s $PWD/config/zsh $HOME/.zshrc  # (still) using bash atm
 ln -s $PWD/config/tmux $HOME/.tmux.conf
 ln -s $PWD/config/neovim $HOME/.config/nvim/init.vim
+ln -s $PWD/config/gitconfig $HOME/.gitconfig
 
 # Fix CTRL-H
 ln -s $PWD/xterm-256color.ti $HOME/xterm-256color.ti
